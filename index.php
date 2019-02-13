4135c39f73068e635ceeff5267fef9ceaffa8a3e
vitor pires <vitn47@gmail.com>
2019-02-13 17:09:06 -0200

    nova modificação

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
+$user = new Usuario();
+$user->loadbyid(6);
+echo $user;
  ?>
